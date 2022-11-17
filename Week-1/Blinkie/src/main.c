11f122a49d53e73a159661a9e36fff44329073a4
Melvin Moes <melvin.moes@hva.nl>
2022-11-17 12:14:31 +0100

    Started blinkie code

--- Week-1/Blinkie/src/main.c
+++ Week-1/Blinkie/src/main.c
@@ -1,1 +1,19 @@
-void app_main() {}
+//Importing gpio drivers
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+//Defining the Led Pin 
+#define LED_PIN 21
+
+//Main code
+void app_main() {
+    //Setting power direction
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+    //Turning Led on
+    gpio_set_level(LED_PIN, HIGH);
+    //Adding a delay between turning the Led on and off
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // 1000 milliseconde = 1 seconde
+    //Turning Led off
+    gpio_set_level(LED_PIN, LOW);
+}
