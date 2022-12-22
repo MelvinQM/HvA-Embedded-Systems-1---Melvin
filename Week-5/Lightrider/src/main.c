a67479a094e15a9ce4cc4bc78dda9343fa4eabd3
Melvin Moes <melvin.moes@hva.nl>
2022-12-22 13:51:30 +0100

    Lightrider code afgemaakt (Het werkt!)

--- Week-5/Lightrider/src/main.c
+++ Week-5/Lightrider/src/main.c
@@ -4,19 +4,22 @@ Melvin Moes
 Lightrider
 */
 //Importing gpio drivers
+#include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
+#include "driver/adc.h"
 
 //Defining the Led Pins
 int LED_PIN[10] = {4, 5, 6, 7, 15, 16, 17, 18, 8, 3};
-int POT_PIN = 2 //Phys 1 ADC 1_1
 #define HIGH 1
 #define LOW 0
 #define DELAY 1000 // 1000 milliseconde = 1 seconde
 
 //Main code
 void app_main() {
+    //Config for adc
+    adc1_config_width(10);
+    adc1_config_channel_atten(ADC1_CHANNEL_1, ADC_ATTEN_DB_11);
 
     //Setting power direction for every single led
     for (size_t i = 0; i < 10; i++)
@@ -28,41 +31,49 @@ void app_main() {
     int waarde = 0b000000001;
 
     while (true){
-    //Initial direction
-    for (size_t i = 0; i < 10; i++)
-    {
-        for (size_t j = 0; j < 10; j++)
-        {   
-            //De waarde is 
-            if ((waarde >> j) & 1){
-                gpio_set_level(LED_PIN[j], HIGH);
-            } else {
-                gpio_set_level(LED_PIN[j], LOW);
+        int delay;
+        //Initial direction
+        for (size_t i = 0; i < 10; i++)
+        {
+            //Read value from potentiometer and add it as delay
+            delay = adc1_get_raw(ADC1_CHANNEL_1);
+            vTaskDelay(delay / portTICK_PERIOD_MS);
+            for (size_t j = 0; j < 10; j++)
+            {   
+                printf("Potentiometer value: %d\n", delay);
+                //De waarde is 
+                if ((waarde >> j) & 1){
+                    gpio_set_level(LED_PIN[j], HIGH);
+                } else {
+                    gpio_set_level(LED_PIN[j], LOW);
+                }
+            }
+            if (i < 9){
+                waarde = waarde << 1;
             }
         }
-        if (i < 9){
-            waarde = waarde << 1;
-        }
-        vTaskDelay(DELAY / portTICK_PERIOD_MS);
-    }
+        //Opposite direction
+        for (size_t i = 0; i < 10; i++)
+        {
+            //Read value from potentiometer and add it as delay
+            delay = adc1_get_raw(ADC1_CHANNEL_1);
+            vTaskDelay(delay / portTICK_PERIOD_MS);
 
-    //Opposite direction
-    for (size_t i = 0; i < 10; i++)
-    {
-        for (size_t j = 0; j < 10; j++)
-        {   
-            //De waarde is 
-            if ((waarde >> j) & 1){
-                gpio_set_level(LED_PIN[j], HIGH);
-            } else {
-                gpio_set_level(LED_PIN[j], LOW);
+            for (size_t j = 0; j < 10; j++)
+            {   
+                printf("Potentiometer value: %d\n", delay);
+                //De waarde is 
+                if ((waarde >> j) & 1){
+                    gpio_set_level(LED_PIN[j], HIGH);
+                } else {
+                    gpio_set_level(LED_PIN[j], LOW);
+                }
             }
+            if (i < 9){
+                waarde = waarde >> 1;
+            }
+            
         }
-        if (i < 9){
-            waarde = waarde >> 1;
-        }
-        vTaskDelay(DELAY / portTICK_PERIOD_MS);
-    }
     }
     
     
